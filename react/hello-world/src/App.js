ee9f47b934c8a9ffe364b8273310eb68f579152f
Terrian Heck <terrianheck@Terrians-MacBook-Air.local>
2023-05-23 19:49:51 -0400

    adding new files

--- react/hello-world/src/App.js
+++ react/hello-world/src/App.js
@@ -2,19 +2,12 @@ import logo from "./logo.svg";
 import "./App.css";
 import { Greet, Greet2, Greet3 } from "./components/Greet.js";
 import { Welcome, Welcome2, Welcome3 } from "./components/Welcome.js";
+import Message from "./components/Message.js";
 
 function App() {
   return (
     <div className="App">
-      <Greet name="Mike" nickname="Myers" />
-      <button> Come on in</button>
-      <p>this is a brand new paragraph for the example of this website.</p>
-      <Greet name="John" nickname="Deer" />
-      <Greet2 name="Mike" nickname="Myers" />
-      <Greet3 name="John" nickname="Deer" />
-      <Welcome name="Terrian" nickname="TJ" />
-      <Welcome2 name="Valerie" nickname="Althea" />
-      <Welcome3 />
+      <Message />
     </div>
   );
 }
